e360065795302d79c99470f6957bbad1a7bc1f53
AlissCava <cavalleraalissa@gmail.com>
2024-01-23 15:31:03 +0100

    stampe hotel

---
+++ hotel.php
@@ -0,0 +1,51 @@
+<?php
+
+$hotels = [
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+];
+
+// Stampare tutti gli hotel con tutti i dati disponibili
+foreach ($hotels as $hotel) {
+    echo "Name: {$hotel['name']}\n";
+    echo "Description: {$hotel['description']}\n";
+    echo "Parking: " . ($hotel['parking'] ? 'Yes' : 'No') . "\n";
+    echo "Vote: {$hotel['vote']}\n";
+    echo "Distance to Center: {$hotel['distance_to_center']} km\n";
+    echo "-----------------\n";
+}
+
+?>
